466d05ce614da7434f6ada4b6692d37131785c0a
Line_cut_feng <932413237@qq.com>
2019-07-01 10:40:16 +0800

    初步创建项目，更改已做题目的包名，算法题解以及题目描述待更新

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello LeetCode!");
+        System.out.println("从今以后无感情，只做一个刷题怪!");
+    }
+}
---
+++ src/Problem18_四数之和/Solution.java
@@ -0,0 +1,22 @@
+package Problem18_四数之和;
+
+import java.util.HashMap;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] r = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                r[0] = i;
+                r[1] = map.get(target - nums[i]);
+                return r;
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[]{0, 0};
+    }
+}
+
