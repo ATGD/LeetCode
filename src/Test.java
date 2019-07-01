2bc6e87c390ec55a0dc292ce031e3e3287017277
Line_cut_feng <932413237@qq.com>
2019-07-01 13:28:51 +0800

    规范化文件夹命名

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import java.text.DecimalFormat;
-
-public class Test {
-
-    public static void main(String[] args) {
-        String format = new DecimalFormat("0000").format(Integer.parseInt("1045"));
-        System.out.println(format);
-    }
-}
