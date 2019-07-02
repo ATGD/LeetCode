db8d1b6bd2bbaaf90dabdf62882c5763895fba09
Line_cut_feng <932413237@qq.com>
2019-07-02 11:01:06 +0800

    规范化文件夹命名

--- src/Problem_0002_两数相加/Solution.java
+++ src/Problem_0002_两数相加/Solution.java
@@ -1,22 +1,44 @@
 package Problem_0002_两数相加;
 
-import java.util.HashMap;
-
 public class Solution {
 
-    public int[] twoSum(int[] nums, int target) {
-        int[] r = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                r[0] = i;
-                r[1] = map.get(target - nums[i]);
-                return r;
-            }
-            map.put(nums[i], i);
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int value = l1.val + l2.val;
+        boolean flag = false;
+        if (value >= 10) {
+            value -= 10;
+            flag = true;
         }
+        ListNode l3 = new ListNode(value);
+        ListNode l4 = l3;
+        while (l1.next != null || l2.next != null) {
+            l1 = l1.next == null ? new ListNode(0) : l1.next;
+            l2 = l2.next == null ? new ListNode(0) : l2.next;
+            value = l1.val + l2.val;
+            if (flag) {
+                value++;
+            }
+            if (value >= 10) {
+                value -= 10;
+                flag = true;
+            } else {
+                flag = false;
+            }
+            l4 = l4.next = new ListNode(value);
+        }
+        if (flag) {
+            l4 = l4.next = new ListNode(1);
+        }
+        return l3;
+    }
+}
 
-        return new int[]{0, 0};
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
     }
 }
 
--- src/Problem_0005_最长回文子串/Solution.java
+++ src/Problem_0005_最长回文子串/Solution.java
@@ -2,21 +2,54 @@ package Problem_0005_最长回文子串;
 
 import java.util.HashMap;
 
-public class Solution {
-
-    public int[] twoSum(int[] nums, int target) {
-        int[] r = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                r[0] = i;
-                r[1] = map.get(target - nums[i]);
-                return r;
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0)
+            return "";
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length() - 1; j > i; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    boolean isPalindrome = true;
+                    int tempLeft = i;
+                    int tempRight = j;
+                    int length = j - i + 1;
+                    if (length % 2 == 1) {
+                        while ((tempRight - tempLeft) != 2) {
+                            tempLeft++;
+                            tempRight--;
+                            if (s.charAt(tempLeft) != s.charAt(tempRight)) {
+                                isPalindrome = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        while ((tempRight - tempLeft) != 1) {
+                            tempLeft++;
+                            tempRight--;
+                            if (s.charAt(tempLeft) != s.charAt(tempRight)) {
+                                isPalindrome = false;
+                                break;
+                            }
+                        }
+                    }
+                    if ((length > max) && isPalindrome) {
+                        start = i;
+                        end = j;
+                        max = length;
+                        if (max == s.length()) {
+                            return s;
+                        }
+                    }
+                }
             }
-            map.put(nums[i], i);
         }
-
-        return new int[]{0, 0};
+        if (max == 0) {
+            return s.substring(0, 1);
+        }
+        return s.substring(start, end + 1);
     }
 }
 
