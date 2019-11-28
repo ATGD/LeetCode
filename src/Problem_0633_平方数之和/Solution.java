db258a65c47df1a9d1bb5abfbfbec51128786d59
932413237 <932413237@qq.com>
2019-11-28 10:00:01 +0800

    0663 0382 0284 0633

--- src/Problem_0633_平方数之和/Solution.java
+++ src/Problem_0633_平方数之和/Solution.java
@@ -1,8 +1,13 @@
-package Problem_0663_平方数之和;
+package Problem_0633_平方数之和;
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b)
+                return true;
+        }
+        return false;
     }
 }
 
