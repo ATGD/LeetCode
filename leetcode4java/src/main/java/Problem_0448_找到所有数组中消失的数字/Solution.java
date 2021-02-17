ebf3cdbde0a1a954d6e6bf1a53d8dbedf864017e
fenghongxiang <fenghongxiang@xiaomi.com>
2021-02-17 22:15:24 +0800

    2021.02.17 提交1379

--- leetcode4java/src/main/java/Problem_0448_找到所有数组中消失的数字/Solution.java
+++ leetcode4java/src/main/java/Problem_0448_找到所有数组中消失的数字/Solution.java
@@ -5,25 +5,21 @@ import java.util.List;
 
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        for (int i = 0; i < nums.length; ) {
-            while (nums[i] != i + 1 && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
-            }
-            i++;
-        }
-        List<Integer> numList = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i + 1) {
-                numList.add(i + 1);
-            }
+            while ((nums[i] != i + 1) && (nums[i] != nums[nums[i] - 1]))
+                swap(nums, i, nums[i] - 1);
         }
-        return numList;
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] != i + 1) list.add(i + 1);
+        return list;
     }
 
     private void swap(int[] nums, int indexA, int indexB) {
-        nums[indexA] ^= nums[indexB];//c
-        nums[indexB] ^= nums[indexA];//a
-        nums[indexA] ^= nums[indexB];
+        int C = nums[indexA];
+        nums[indexA] = nums[indexB];
+        nums[indexB] = C;
     }
 }
 
@@ -31,7 +27,8 @@ class Test {
 
     public static void main(String[] args) {
         int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
-        new Solution().findDisappearedNumbers(nums);
+        List<Integer> disappearedNumbers = new Solution().findDisappearedNumbers(nums);
+        System.out.println(disappearedNumbers);
     }
 }
 
