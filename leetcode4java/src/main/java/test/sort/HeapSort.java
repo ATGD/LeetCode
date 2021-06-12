5a28755d4f39749fe5bccd3ff139af38f22606dd
fenghongxiang <fenghongxiang@xiaomi.com>
2021-06-12 11:04:56 +0800

    [Feature]提交若干题目
    Signed-off-by: fenghongxiang <fenghongxiang@xiaomi.com>

--- leetcode4java/src/main/java/test/sort/HeapSort.java
+++ leetcode4java/src/main/java/test/sort/HeapSort.java
@@ -3,7 +3,10 @@ package test.sort;
 public class HeapSort {
 
     public int[] sortArray(int[] nums) {
-        for (int i = (nums.length >> 1) - 1; i >= 0; i--) adustHeap(nums, i, nums.length);
+
+        for (int i = (nums.length >> 1) - 1; i >= 0; i--)
+            adustHeap(nums, i, nums.length);
+
         for (int i = nums.length - 1; i >= 0; i--) {
             swap(nums, 0, i);
             adustHeap(nums, 0, i);
@@ -12,19 +15,24 @@ public class HeapSort {
     }
 
     private void adustHeap(int[] nums, int i, int length) {
+
         int tmp = nums[i];
         for (int j = (i << 1) + 1; j < length; j = (j << 1) + 1) {
-            if (j + 1 < length && nums[j + 1] > nums[j]) j++;
+            if (j + 1 < length && nums[j + 1] > nums[j])
+                j++;
+
             if (nums[j] > tmp) {
                 nums[i] = nums[j];
                 i = j;
-            } else break;
+            } else
+                break;
         }
         nums[i] = tmp;
     }
 
 
     void swap(int[] nums, int i, int j) {
+
         if (nums[i] == nums[j]) return;
         nums[i] ^= nums[j];
         nums[j] ^= nums[i];
